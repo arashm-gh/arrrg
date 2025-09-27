bec7c09c46a7335fd3d2282beac608a948160878
CyanSpaghetti <tp.arash.m@gmail.com>
2025-09-28 01:12:26 +0500

    Install script fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 VERSION="v1.0.0"
-URL="https://github.com/arashm-gh/arrrg/releases/download/$VERSION$/arrrg-$VERSION$-x86_64.tar.gz"
+URL="https://github.com/arashm-gh/arrrg/releases/download/$VERSION$/arrrg-$VERSION$-x86_64-unknown-linux.tar.gz"
 curl -L "$URL$" | tar -xz
 sudo mv arrrg /usr/local/bin/
 ehco "Installed arrrg! Try running arrrg <query> <limit>"
