4dfd49a33a461d2b4c3698b9c65dbe774db81637
CyanSpaghetti <tp.arash.m@gmail.com>
2025-09-28 01:24:02 +0500

    Install script fix 5

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 VERSION="v1.0.0"
-URL="https://github.com/arashm-gh/arrrg/releases/download/cli/arrrg.tar.gz"
-curl -L "$URL$" | tar -xz
+URL="https://github.com/arashm-gh/arrrg/releases/download/$VERSION/arrrg-$VERSION-x86_64-unknown-linux-musl.tar.gz"
+
+# Download and extract
+curl -L "$URL" -o arrrg.tar.gz
+tar -xzf arrrg.tar.gz
+
+# Move to /usr/local/bin
 sudo mv arrrg /usr/local/bin/
-ehco "Installed arrrg! Try running arrrg <query> <limit>"
+rm arrrg.tar.gz
+
+echo "Installed arrrg! Try running: arrrg <query> <limit>"
